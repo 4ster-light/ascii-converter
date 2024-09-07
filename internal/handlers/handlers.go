85c8e993ca998fa487ea9cf8dd02540b7a9ea85e
King-kin5 <108242144+King-kin5@users.noreply.github.com>
2024-09-07 22:50:17 +0100

    push

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,12 +10,9 @@ import (
 	"path/filepath"
 )
 
-<<<<<<< HEAD
-=======
 const maxMemory = 10 << 20 // 10 MB
 
 // Presents the page and has a form to upload an image
->>>>>>> baa14539c471507f6a1f7dee252b4f5415d3aaba
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	templates.Home("✰ArtSCII✰").Render(r.Context(), w)
 }
